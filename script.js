31239f6336901e2547a4a7437991717012d8539b
Ian Yake <iangyake@gmail.com>
2020-12-23 22:31:56 -0600

    remove specials menu

--- script.js
+++ script.js
@@ -9,16 +9,16 @@ function hide(elementById) {
 
 // Wait until window is loaded to hide specials.
 // Otherwise these elements can't be found.
-// window.onload = function hideSpecials() {
-//   hide('specials-tab');
-//   hide('specials');
-//
-//   breakfast_tab = document.getElementById('breakfast-tab');
-//   breakfast = document.getElementById('breakfast');
-//
-//   // selects the breakfast tab, making it appear white
-//   breakfast_tab.classList.add('active');
-//
-//   // displays the breakfast menu
-//   breakfast.className = 'tab-pane fade in active';
-// }
+window.onload = function hideSpecials() {
+  hide('specials-tab');
+  hide('specials');
+
+  breakfast_tab = document.getElementById('breakfast-tab');
+  breakfast = document.getElementById('breakfast');
+
+  // selects the breakfast tab, making it appear white
+  breakfast_tab.classList.add('active');
+
+  // displays the breakfast menu
+  breakfast.className = 'tab-pane fade in active';
+}
