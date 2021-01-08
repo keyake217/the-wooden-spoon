6a77d3035e3d08707cbad9b42da2f056ef006a87
Ian Yake <iangyake@gmail.com>
2021-01-08 14:09:00 -0600

    add function scheduler and hide announcement

--- script.js
+++ script.js
@@ -1,15 +1,40 @@
+// run multiple funcs when the page loads
+window.onload = function (event) {
+  // schedule(showAnnouncement, '01-08-2021','01-09-2021', 'Test announcement message 2');
+  hideSpecials();
+}
+
 function hide(elementById) {
     var element = document.getElementById(elementById);
     element.style.display = "none";
 }
 
+function show(elementById) {
+    var element = document.getElementById(elementById);
+    element.style.display = "block";
+}
 
+function showAnnouncement(message) {
+    element = document.getElementById('announcement-message');
+    element.innerHTML = message
+    show('announcement');
+}
 
-//// Uncomment below to hide specials menu
+// func: function that we want to run during specific dates
+// dateFrom: YYYY-MM-DD formatted string
+// dateTo: YYYY-MM-DD formatted string
+// ...args: to pass arbitrary number of arguments to func
+function schedule(func, dateFromStr, dateToStr, ...args) {
+  let currDate = new Date();
+  let dateFrom = new Date(dateFromStr);
+  let dateTo = new Date(dateToStr);
 
-// Wait until window is loaded to hide specials.
-// Otherwise these elements can't be found.
-window.onload = function hideSpecials() {
+  if (currDate >= dateFrom && currDate <= dateTo) {
+    func(...args);
+  }
+}
+
+function hideSpecials() {
   hide('specials-tab');
   hide('specials');
 
